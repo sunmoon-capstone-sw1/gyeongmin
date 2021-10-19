c093c63728041e19df5949504b6603419395919f
gyeongmin <gyeongmin_330@naver.com>
2021-10-20 01:36:44 +0900

    make mobilenet model ver.02

--- triton_infer/call_refactored_image_client.py
+++ triton_infer/call_refactored_image_client.py
@@ -1,6 +1,6 @@
 import refactoring_image_client
 
-test_img_path = "C:/Users/2019A00298/test_img_before_make_model/dollor_50.jpg"
+test_img_path = "C:/Users/2019A00298/test_img_before_make_model/dollor_1.jpg"
 model_name = "mobilenet_money_detection"
 
 t = refactoring_image_client.Triton(model_name, test_img_path)
